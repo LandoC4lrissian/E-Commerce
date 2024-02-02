575a37c9df49723196a273ecd8d01d51a69c6110
LandoC4lrissian <ahmetatylz55@gmail.com>
2024-02-03 02:09:08 +0300

    add-product page.tsx doc designed

--- nextjs-ecommerce/app/add-product/page.tsx
+++ nextjs-ecommerce/app/add-product/page.tsx
@@ -2,6 +2,36 @@ export default function AddProductPage() {
   return (
     <div>
       <h1 className="text-lg mb-3 font-bold">Add Product</h1>
+      <form>
+        <input
+          required
+          name="name"
+          placeholder="Name"
+          className="mb-3 w-full input imput-bordered"
+        />
+        <textarea
+          required
+          name="description"
+          placeholder="Description"
+          className="textarea-bordered textarea mb-3 w-full"
+        />
+        <input
+          required
+          name="imageUrl"
+          placeholder="Image URL"
+          type="url"
+          className="mb-3 w-full input imput-bordered"
+        />
+        <input
+          required
+          name="price"
+          placeholder="Price"
+          type="number"
+          className="mb-3 w-full input imput-bordered"
+        />
+        <button className="btn btn-primary btn-block" type="submit">Add Product</button>
+        
+      </form>
     </div>
-    );
+  );
 }
