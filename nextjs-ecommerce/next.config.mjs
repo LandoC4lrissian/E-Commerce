40973708038b162236808469dc13ae47332cf10b
LandoC4lrissian <ahmetatylz55@gmail.com>
2024-02-03 03:05:15 +0300

    next.config.mjs doc editted

--- nextjs-ecommerce/next.config.mjs
+++ nextjs-ecommerce/next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    images: {
+        remotePatterns: [{hostname: "image.unsplash.com"}],
+    },/*remotePatterns  uygulamanızda hangi uzaktan URL'lerden gelen görsellerin optimize edileceğini ve işleneceğini belirlemenizi sağlar.
+    Bu sayede, Next.js'in Görüntü Optimizasyonu API'sini kullanarak performansı ve kullanıcı deneyimini iyileştirebilirsiniz.*/
   experimental: {
     serverActions: true,
   },
