7d801028371d2daf1a0ff973745f0e299d634193
LandoC4lrissian <ahmetatylz55@gmail.com>
2024-02-04 15:36:45 +0300

    FormSubmitButton dosyasına props eklendi

--- nextjs-ecommerce/app/components/FormSubmitButton.tsx
+++ nextjs-ecommerce/app/components/FormSubmitButton.tsx
@@ -1,34 +1,40 @@
 "use client";
 
-import { ComponentProps} from "react";
+import { ComponentProps } from "react";
 import { experimental_useFormStatus as useFormStatus } from "react-dom";
 
 type FormSubmitButtonProps = {
-    children: React.ReactNode,
-    //React.ReactNode, React'te herhangi bir geçerli içeriği temsil eden bir türdür. Bu, metin, sayılar, DOM elementleri, React bileşenleri ve bunların dizileri olabilir.
-    className?: string,
-    /*className?: Bu prop, FormSubmitButton bileşeninin className özelliğini tanımlar.
+  children: React.ReactNode;
+  //React.ReactNode, React'te herhangi bir geçerli içeriği temsil eden bir türdür. Bu, metin, sayılar, DOM elementleri, React bileşenleri ve bunların dizileri olabilir.
+  className?: string;
+  /*className?: Bu prop, FormSubmitButton bileşeninin className özelliğini tanımlar.
     Bu özellik, bileşene bir CSS sınıfı atamak için kullanılır. ? işareti, prop'un opsiyonel olduğunu gösterir.*/
 } & ComponentProps<"button">; // Bu kısım, FormSubmitButton bileşeninin button HTML etiketinin tüm özelliklerini de desteklediğini gösterir.
 
-export default function FormSubmitButton(
-    {children,className} : FormSubmitButtonProps
-) {
-    const {pending} = useFormStatus();
+export default function FormSubmitButton({
+  children,
+  className,
+  ...props
+}: FormSubmitButtonProps) {
+  const { pending } = useFormStatus();
 
-    return(
-        <button
-        className={`btn btn-primary ${className}`}
-        type="submit" /*type özelliği, düğmenin türünü belirtir. Bu durumda, submit değeri kullanılır.
+  return (
+    <button
+    {...props} //props, FormSubmitButton bileşenine geçirilen tüm özellikleri içerir. Bu, onClick, type, disabled gibi özellikleri içerebilir.
+      className={`btn btn-primary ${className}`}
+      type="submit" /*type özelliği, düğmenin türünü belirtir. Bu durumda, submit değeri kullanılır.
         Bu, düğmeye tıklandığında formdaki verilerin sunucuya gönderileceği anlamına gelir.*/
-        disabled={pending}/*disabled özelliği, düğmenin devre dışı olup olmadığını belirler. pending değişkeninin değeri true ise düğme devre dışı bırakılır.
+      disabled={
+        pending
+      } /*disabled özelliği, düğmenin devre dışı olup olmadığını belirler. pending değişkeninin değeri true ise düğme devre dışı bırakılır.
         Bu, form gönderilirken kullanıcının birden fazla kez tıklamasını önler.*/
-        /*pending değişkeni, useFormStatus kancasından alınır.
+      /*pending değişkeni, useFormStatus kancasından alınır.
         Bu değişken, form gönderme işleminin durumunu gösterir. true ise form gönderiliyor, false ise form gönderilmiyor demektir.*/
-        >
-            {pending && <span className="loading loading-spinner" />}
-            {/*pending değişkeni true ise, yani form gönderiliyorsa, loading sınıfı ve loading-spinner sınıfı içeren bir span elementi gösterilir.*/}
-            {children}</button>
-            //children prop'u, FormSubmitButton bileşeninin içeriğini temsil eder. Bu durumda, düğmenin metni.
-    )
+    >
+      {pending && <span className="loading loading-spinner" />}
+      {/*pending değişkeni true ise, yani form gönderiliyorsa, loading sınıfı ve loading-spinner sınıfı içeren bir span elementi gösterilir.*/}
+      {children}
+    </button>
+    //children prop'u, FormSubmitButton bileşeninin içeriğini temsil eder. Bu durumda, düğmenin metni.
+  );
 }
