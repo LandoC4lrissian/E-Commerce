5a9a1ec9e2b491becfce83409bd3089d42b0f531
LandoC4lrissian <ahmetatylz55@gmail.com>
2024-02-05 02:00:49 +0300

    not-found.tsx page added and products page.tsx edited.

--- nextjs-ecommerce/app/products/[id]/page.tsx
+++ nextjs-ecommerce/app/products/[id]/page.tsx
@@ -1,16 +1,43 @@
-import {prisma} from "@/lib/db/prisma";
+import { prisma } from "@/lib/db/prisma";
+import { notFound } from "next/navigation";
+import Image from "next/image";
+import PriceTag from "@/components/PriceTag";
+
 
 interface ProductPageProps {
-    params: { 
-        id: string,
-    }
-    // params: { ... }: URL'den alınan parametreleri içeren bir nesne.
+  params: {
+    id: string;
+  };
+  // params: { ... }: URL'den alınan parametreleri içeren bir nesne.
 }
 
 export default async function ProductPage(
-    {params: {id}} : ProductPageProps
-    // {params: {id}}: URL'den alınan "id" parametresini alır.
-){
-    const product = await prisma.product.findUnique({where: {id}})
-    // product.findUnique: "product" adlı veritabanı tablosundan yalnızca bir kayıt almak için özel bir Prisma Client yöntemidir.
+  { params: { id } }: ProductPageProps
+  // {params: {id}}: URL'den alınan "id" parametresini alır.
+) {
+  const product = await prisma.product.findUnique({ where: { id } });
+  // product.findUnique: "product" adlı veritabanı tablosundan yalnızca bir kayıt almak için özel bir Prisma Client yöntemidir.
+  // where: findUnique seçenek nesnesi içinde alınacak kaydı belirlemek için bir seçenek.
+
+  if (!product) notFound();
+  // Eğer ürün bulunamazsa, 404 sayfası gösterilir.
+
+  return (
+    <div className="flex flex-col lg:flex-row gap-4 lg:items-center">
+      <Image
+        src={product.imageUrl}
+        alt={product.name}
+        width={500}
+        height={500}
+        className="rounded-lg"
+        priority
+      />
+
+      <div>
+        <h1 className="text-5xl font-bold">{product.name}</h1>
+        <PriceTag price={product.price} className="mt-4"/>
+        <p className="py-6">{product.description}</p>
+      </div>
+    </div>
+  );
 }
