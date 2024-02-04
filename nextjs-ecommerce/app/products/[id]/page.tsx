be9968a7d442df9ada51ec63ab8b50b9865e1d70
LandoC4lrissian <ahmetatylz55@gmail.com>
2024-02-05 02:25:55 +0300

    page.tsx doc edited which is in products folder

--- nextjs-ecommerce/app/products/[id]/page.tsx
+++ nextjs-ecommerce/app/products/[id]/page.tsx
@@ -2,7 +2,8 @@ import { prisma } from "@/lib/db/prisma";
 import { notFound } from "next/navigation";
 import Image from "next/image";
 import PriceTag from "@/app/components/PriceTag";
-
+import { Metadata } from "next";
+import { cache } from "react";
 
 interface ProductPageProps {
   params: {
@@ -11,16 +12,40 @@ interface ProductPageProps {
   // params: { ... }: URL'den alınan parametreleri içeren bir nesne.
 }
 
-export default async function ProductPage(
-  { params: { id } }: ProductPageProps
-  // {params: {id}}: URL'den alınan "id" parametresini alır.
-) {
+const getProduct = cache(async (id: string) => {
   const product = await prisma.product.findUnique({ where: { id } });
   // product.findUnique: "product" adlı veritabanı tablosundan yalnızca bir kayıt almak için özel bir Prisma Client yöntemidir.
   // where: findUnique seçenek nesnesi içinde alınacak kaydı belirlemek için bir seçenek.
 
   if (!product) notFound();
   // Eğer ürün bulunamazsa, 404 sayfası gösterilir.
+
+  return product;
+});
+// cache: Bu işlevin sonuçlarını önbelleğe alır ve aynı parametrelerle tekrar çağrıldığında önbellekten sonuçları döndürür.
+
+export async function generateMetadata({
+  params: { id }, // params: {id}: URL'den alınan "id" parametresini alır.
+}: ProductPageProps): Promise<Metadata> {
+  // Metadata: Sayfa başlığı, açıklaması ve sosyal medya paylaşımı için resimler gibi sayfa meta verilerini içeren bir nesne.
+  const product = await getProduct(id);
+  // getProduct: Ürün bilgilerini almak için kullanılan bir işlev.
+
+  return {
+    title: product.name + " - Flowmazon", // Sayfa başlığı.
+    description: product.description, // Sayfa açıklaması.
+    openGraph: {
+      images: [{ url: product.imageUrl }],
+    },
+    // openGraph: Sosyal medya paylaşımı için ürünün resim URL'sini içeren bir nesne.
+  };
+}
+
+export default async function ProductPage(
+  { params: { id } }: ProductPageProps
+  // {params: {id}}: URL'den alınan "id" parametresini alır.
+) {
+  const product = await getProduct(id);
 
   return (
     <div className="flex flex-col lg:flex-row gap-4 lg:items-center">
@@ -35,7 +60,7 @@ export default async function ProductPage(
 
       <div>
         <h1 className="text-5xl font-bold">{product.name}</h1>
-        <PriceTag price={product.price} className="mt-4"/>
+        <PriceTag price={product.price} className="mt-4" />
         <p className="py-6">{product.description}</p>
       </div>
     </div>
