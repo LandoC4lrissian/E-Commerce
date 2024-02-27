3daa90123413a2c2c17a5d81255270de350f0e98
LandoC4lrissian <ahmetatylz55@gmail.com>
2024-02-27 12:51:32 +0300

    cart.ts dosyası güncellendi

--- nextjs-ecommerce/lib/db/cart.ts
+++ nextjs-ecommerce/lib/db/cart.ts
@@ -1,23 +1,52 @@
 import { cookies } from "next/headers";
 import { prisma } from "./prisma";
+import { Cart, Prisma } from "@prisma/client";
 
-export async function getCart() {
-    const localCartId = cookies().get("localCartId")?.value;
-    const cart =localCartId ?
-    await prisma.cart.findUnique({
+export type CartWithProducts = Prisma.CartGetPayload<{
+  include: { items: { include: { product: true } } };
+}>;
+
+export type ShoppingCart = CartWithProducts & {
+  size: number;
+  subtotal: number;
+};
+
+export async function getCart(): Promise<ShoppingCart | null> {
+  const localCartId = cookies().get("localCartId")?.value;
+  const cart = localCartId
+    ? await prisma.cart.findUnique({
         where: { id: localCartId },
-        include: { items : { include: { product: true } } }
-    }) 
+        include: { items: { include: { product: true } } },
+      })
     : null;
+
+  if (!cart) {
+    return null;
+  }
+
+  return {
+    ...cart,
+    size: cart.items.reduce((acc, item) => acc + item.quantity, 0),
+    subtotal: cart.items.reduce(
+      (acc, item) => acc + item.quantity * item.product.price,
+      0
+    ),
+  };
 }
 
-export async function createCart() {
+export async function createCart(): Promise<ShoppingCart> {
   const newCart = await prisma.cart.create({
-    data: {}
+    data: {},
   });
 
   //Needs encryption + secure settings in real production app
   //"localCartId" adında bir çerez oluşturulur ve yeni sepetin kimliği bu çereze yazılır.
-  cookies().set("localCartId", newCart.id,)
+  cookies().set("localCartId", newCart.id);
 
+  return {
+    ...newCart,
+    items: [],
+    size: 0,
+    subtotal: 0,
+  };
 }
